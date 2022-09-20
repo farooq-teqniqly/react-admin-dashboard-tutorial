55762d0451ca080fd84d9ceb73ebd45e9ff35c39
Farooq <farooq@teqniqly.com>
2022-09-19 22:59:16 -0700

    Navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,54 @@
 import React from "react";
 import "./navbar.scss";
+import SearchIcon from "@mui/icons-material/Search";
+import LanguageIcon from "@mui/icons-material/Language";
+import DarkModeIcon from "@mui/icons-material/DarkMode";
+import FullscreenExitIcon from "@mui/icons-material/FullscreenExit";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import ChatBubbleIcon from "@mui/icons-material/ChatBubble";
+import ListIcon from "@mui/icons-material/List";
 
 function Navbar() {
-  return <div className="navbar">Navbar</div>;
+  return (
+    <div className="navbar">
+      <div className="wrapper">
+        <div className="search">
+          <input type="text" placeholder="Search..." />
+          <SearchIcon></SearchIcon>
+        </div>
+        <div className="items">
+          <div className="item">
+            <LanguageIcon className="icon"></LanguageIcon>
+            English
+          </div>
+          <div className="item">
+            <DarkModeIcon className="icon"></DarkModeIcon>
+          </div>
+          <div className="item">
+            <FullscreenExitIcon className="icon"></FullscreenExitIcon>
+          </div>
+          <div className="item">
+            <NotificationsNoneIcon className="icon"></NotificationsNoneIcon>
+            <div className="counter">4</div>
+          </div>
+          <div className="item">
+            <ChatBubbleIcon className="icon"></ChatBubbleIcon>
+            <div className="counter">1</div>
+          </div>
+          <div className="item">
+            <ListIcon className="icon"></ListIcon>
+          </div>
+          <div className="item">
+            <img
+              src="https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?cs=srgb&dl=pexels-simon-robben-614810.jpg&fm=jpg"
+              alt=""
+              className="avatar"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Sidebar from "../../components/sidebar/Sidebar";
+import Navbar from "../../components/navbar/Navbar";
 import "./home.scss";
 
 const Home = () => {
   return (
     <div className="home">
       <Sidebar></Sidebar>
-      <div className="homeContainer">container</div>
+      <div className="homeContainer">
+        <Navbar></Navbar>
+        home container
+      </div>
     </div>
   );
 };
