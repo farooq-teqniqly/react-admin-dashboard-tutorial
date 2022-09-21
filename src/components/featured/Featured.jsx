30586c4645d1c9954081eb9300c53027b75a1411
Farooq <farooq@teqniqly.com>
2022-09-20 20:06:23 -0700

    wip

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -3,6 +3,8 @@ import "./featured.scss";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 import { CircularProgressbar } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
+import ArrowUpwardIcon from "@mui/icons-material/ArrowUpward";
+import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
 
 function Featured() {
   return (
@@ -28,8 +30,23 @@ function Featured() {
         <div className="summary">
           <div className="item">
             <div className="itemTitle">Target</div>
-            <div className="itemResult">
-              <div className="resultAmount">$12.99</div>
+            <div className="itemResult positive">
+              <ArrowUpwardIcon fontSize="small"></ArrowUpwardIcon>
+              <div className="resultAmount">$12.4k</div>
+            </div>
+          </div>
+          <div className="item">
+            <div className="itemTitle">Last Week</div>
+            <div className="itemResult negative">
+              <ArrowDownwardIcon fontSize="small"></ArrowDownwardIcon>
+              <div className="resultAmount">-$1.4k</div>
+            </div>
+          </div>
+          <div className="item">
+            <div className="itemTitle">Last Month</div>
+            <div className="itemResult positive">
+              <ArrowUpwardIcon fontSize="small"></ArrowUpwardIcon>
+              <div className="resultAmount">$12.4k</div>
             </div>
           </div>
         </div>
