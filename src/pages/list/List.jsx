29595072bdd688b6bec10747f4a0d443c1918374
Farooq Mahmud <mahmud.farooq@acuitybrands.com>
2022-09-29 09:15:20 -0700

    wip

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Navbar from "../../components/navbar/Navbar";
 
 const List = () => {
-  return <div>List</div>;
+  return (
+    <div className="list">
+      <Sidebar></Sidebar>
+      <div className="listContainer">
+        <Navbar></Navbar>
+        datatable here
+      </div>
+    </div>
+  );
 };
 
 export default List;
